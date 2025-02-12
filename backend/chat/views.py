77c336693565f4fc9f8495993cff5835d6be2558
Sharathchandrahas <sharathchandrahasreddy@gmail.com>
2025-02-12 19:25:10 +0530

    migrated from old openai

--- backend/chat/views.py
+++ backend/chat/views.py
@@ -3,13 +3,15 @@ import os
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from dotenv import load_dotenv
-import openai  
+from openai import OpenAI
 
 
 load_dotenv()
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-openai.api_key = OPENAI_API_KEY
+client = OpenAI(
+  api_key=OPENAI_API_KEY,  # this is also the default, it can be omitted
+)
 
 @csrf_exempt
 def chat_view(request):
@@ -20,11 +22,11 @@ def chat_view(request):
         conversation.append({"role": "user", "content": user_message})
 
         try:
-            response = openai.ChatCompletion.create(
+            response = client.chat.completions.create(
                 model="gpt-3.5-turbo",
                 messages=conversation
             )
-            bot_response = response['choices'][0]['message']['content']
+            bot_response = response.choices[0].message.content
             conversation.append({"role": "assistant", "content": bot_response})
             return JsonResponse({"response": bot_response, "conversation": conversation})
 
